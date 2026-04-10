083bd3e0000383e7196be3514fb07bdbec6fe8f6
FTMahringer <fynn.mahringer@outlook.com>
2026-04-10 20:08:22 +0200

    fix(nix): adding german key-layout

--- modules/core/base.nix
+++ modules/core/base.nix
@@ -9,6 +9,17 @@
   time.timeZone = "Europe/Vienna";
   i18n.defaultLocale = "de_AT.UTF-8";
 
+  i18n.extraLocaleSettings.LC_ALL = "de_AT.UTF-8";
+
+  # Console keyboard layout (German)
+  console.keyMap = "de";
+
+  # X11 keyboard layout (German) - applies to Wayland too via XKB
+  services.xserver.xkb = {
+    layout = "de";
+    variant = "";
+  };
+
   networking.networkmanager.enable = true;
 
   services.openssh.enable = true;
