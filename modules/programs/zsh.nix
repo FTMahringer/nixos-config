83bc9e80cd6935da191da7735c36e4e37cea843c
FTMahringer <fynn.mahringer@outlook.com>
2026-04-10 20:20:26 +0200

    Update zsh.nix

--- modules/programs/zsh.nix
+++ modules/programs/zsh.nix
@@ -1,26 +1,27 @@
-{ config, pkgs, lib, inputs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
   cfg = config.ft.programs.zsh;
 in
 {
-  imports = [ inputs.nix-your-shell.nixosModules.default ];
-
   options.ft.programs.zsh = {
     enable = lib.mkEnableOption "Zsh";
   };
 
   config = lib.mkIf cfg.enable {
-    programs.zsh.enable = true;
-
-    # nix-your-shell: keeps your shell when using nix shell
-    programs.nix-your-shell = {
+    programs.zsh = {
       enable = true;
-      shell = "zsh";
+      # nix-your-shell: keeps your shell when using nix shell
+      interactiveShellInit = ''
+        if command -v nix-your-shell > /dev/null; then
+          nix-your-shell zsh | source /dev/stdin
+        fi
+      '';
     };
 
     environment.systemPackages = with pkgs; [
       zsh
+      nix-your-shell
     ];
 
     # Make zsh the default shell for users
